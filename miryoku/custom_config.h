92f84e97afb29145456f194cc9f93a625af30cb9
jarjas <56207157+jarjas@users.noreply.github.com>
2025-10-30 21:24:40 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,9 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#pragma once
+//#pragma once
 
-#undef TAPPING_TERM
+//#undef TAPPING_TERM
 #define TAPPING_TERM 400
-#undef U_TAPPING_TERM
+//#undef U_TAPPING_TERM
 #define U_TAPPING_TERM 400
