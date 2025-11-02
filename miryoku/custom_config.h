7316eeb5206d52bf12db4103d9d590e3d23ebbb0
jarjas <56207157+jarjas@users.noreply.github.com>
2025-11-02 01:59:50 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,10 +4,15 @@
 //#pragma once
 
 //#undef TAPPING_TERM
-#define TAPPING_TERM 400
+//#define TAPPING_TERM 400
 //#undef U_TAPPING_TERM
-#define U_TAPPING_TERM 400
+
+
 
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY // For ring finger mods
+
+#include "miryoku.h"
+
+#define U_TAPPING_TERM 800
